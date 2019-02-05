bac4b322db4cef08435ae86faa49f24252dfa4e6
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-02-05 15:56:15 +0300

    included boost library to the project, tested it

--- Zn0wDB/src/main.cpp
+++ Zn0wDB/src/main.cpp
@@ -1,10 +1,31 @@
+// Code was taken for testing purpose from http://programmingexamples.net/wiki/CPP/Boost/Timer
+
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
 
-int main()
+#include <boost/timer.hpp>
+
+void LongFunction(const unsigned int BigNum);
+
+int main(int, char*[])
 {
-	std::cout << "Hello World!" << std::endl;
+	boost::timer timer;
+	LongFunction(1e8);
+	std::cout << timer.elapsed() << " seconds." << std::endl;
+	timer.restart();
+	LongFunction(1e8);
+	std::cout << timer.elapsed() << " seconds." << std::endl;
 
-	system("pause");
-	
 	return 0;
+}
+
+void LongFunction(const unsigned int BigNum)
+{
+	double temp;
+	for (unsigned int i = 0; i < BigNum; i++)
+	{
+		temp = sin(i) / i;
+	}
 }
