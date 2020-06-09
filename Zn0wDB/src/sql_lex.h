3170d6de4e7c763ce164eb81a1b07618a9b5a169
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-06-09 17:12:02 +0300

    working on sql lex analysis

--- Zn0wDB/src/sql_lex.h
+++ Zn0wDB/src/sql_lex.h
@@ -11,7 +11,14 @@ enum TokenType
 	DESCRIBE_STATEMENT,	// shows the structure of the table (its columns)
 	SELECT_STATEMENT,
 	INSERT_STATEMENT,
-	DELETE_STATEMENT
+	DELETE_STATEMENT,
+	UPDATE_STATEMENT,
+	DATABASE_NAME,
+	TABLE_NAME,
+	WHERE_CLAUSE,
+	COLUMN_NAME,
+	VALUE,
+	UNRECOGNIZED
 };
 
 struct Token
@@ -25,15 +32,87 @@ std::vector<Token> lex(std::string& query)
 {
 	std::vector<Token> tokens;
 	
-	char word[256] = "";
-	int c_i = 0, i = 0;
-	int query_length = query.length();
-	while (c_i < query_length)
+	char* query_cp = &query[0];
+	char* word = 0;
+	word = strtok(query_cp, " ()\n\t;");
+	while (word)
 	{
-		if (query.at(i) == ' ' || query.at(i) == '\n' || query.at(i) == '\t')
+		if (strcmp(word, "create") == 0)
 		{
-			if (strcmp(word, "select") == 0)
-				tokens.push_back();
+			word = strtok(0, " ()\n\t;");
+			if (strcmp(word, "database") == 0)
+			{
+				tokens.push_back({ CREATE_DATABASE_STATEMENT, "" });
+				word = strtok(0, " ()\n\t;");
+				tokens.push_back({ DATABASE_NAME, word });
+			}
+			else if (strcmp(word, "table") == 0)
+			{
+				tokens.push_back({ CREATE_TABLE_STATEMENT, "" });
+				word = strtok(0, " ()\n\t;");
+				tokens.push_back({ TABLE_NAME, word });
+			}
+			else
+				tokens.push_back({ UNRECOGNIZED, word });
 		}
+		else if (strcmp(word, "describe") == 0)
+		{
+			tokens.push_back({	DESCRIBE_STATEMENT, "" });
+			word = strtok(0, " ()\n\t;");
+			tokens.push_back({ TABLE_NAME, word });
+		}
+		else if (strcmp(word, "select") == 0)
+		{
+			tokens.push_back({ SELECT_STATEMENT, "" });
+			word = strtok(0, " ()\n\t;");
+			while (word && strcmp(word, "from") != 0)
+			{
+				tokens.push_back({ COLUMN_NAME, word });
+				word = strtok(0, " ()\n\t;");
+			}
+			word = strtok(0, " ()\n\t;");
+			tokens.push_back({ TABLE_NAME, word });
+
+			word = strtok(0, " ()\n\t;");
+			if (word != 0 && strcmp(word, "where") == 0)
+			{
+				word = strtok(0, " ");
+				tokens.push_back({ WHERE_CLAUSE, word });
+			}
+		}
+		else if (strcmp(word, "insert") == 0)
+		{
+			tokens.push_back({ INSERT_STATEMENT, "" });
+			
+			word = strtok(0, " ()\n\t;");
+			if (strcmp(word, "into") != 0)
+			{
+				tokens.push_back({ UNRECOGNIZED, word });
+				continue;
+			}
+
+			word = strtok(0, " ()\n\t;");
+			tokens.push_back({ TABLE_NAME, word });
+
+			word = strtok(0, " ()\n\t;,");
+			while (word && strcmp(word, "values") != 0)
+			{
+				tokens.push_back({ COLUMN_NAME, word });
+				word = strtok(0, " ()\n\t;");
+			}
+
+			word = strtok(0, " ()\n\t;,\'");
+			while (word)
+			{
+				tokens.push_back({ VALUE, word });
+				word = strtok(0, " ()\n\t;");
+			}
+		}
+		else
+		{
+			tokens.push_back({ UNRECOGNIZED, word });
+		}
+
+		word = strtok(0, " ()\n\t;");
 	}
 }
